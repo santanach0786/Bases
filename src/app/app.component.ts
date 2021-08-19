52b2df71259c85b1d847238e45e5706e152ee197
Alejandro Santanach <santanach0786@gmail.com>
2021-08-18 22:29:58 -0400

    Mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title = 'Contador App';
+
+  numero: number = 10;
+
+  base: number = 5;
+
+  acumular(valor: number){
+    this.numero += valor;
+  }
 }
